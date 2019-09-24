5cf333310589c035e0881e02069769eecac2ce94
sungeun532 <55496333+sungeun532@users.noreply.github.com>
2019-09-24 20:38:56 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+
+
+# 뷰함수 임폴트
+
+from mlmodel.controller import freq_note_match
+from mlmodel.controller import note_xml
+from mlmodel.controller import wav_freq_transform
---
+++ main.py
@@ -0,0 +1,45 @@
+import flask
+from flask import Flask, request, render_template
+from sklearn.externals import joblib
+import numpy as np
+from scipy import misc
+import pysynth as ps      # 노트 만들면 wav파일로 저장
+
+app = Flask(__name__)
+app.debug = True
+
+
+# 메인 페이지 라우팅
+@app.route("/")
+@app.route("/upload")
+def index():
+    return render_template('index.html')
+
+@app.route('/uploader', methods=['POST'])
+def wav_freq_transform():
+    if request.method == 'POST':
+        # 업로드 파일 처리 분기
+        f = request.files['music']
+#       if not file: return render_template('index.html', label="No Files")
+
+        # 노트 추출
+        song = controller.NoteConvertor.convert(f)
+
+        # 마르코프 추출
+        matrix = controller.MarcovMatrix(song)
+        start_note = ['c4', 4]
+
+        # 작곡 추출
+        random_song = []
+        for i in range(0, 100):
+                start_note = matrix.next_note(start_note)
+                random_song.append(start_note)
+
+        ps.make_wav(random_song, fn='./random.wav')
+
+        return 
+
+# Rest 등록
+if __name__ == '__main__':
+    # Flask 서비스 스타트
+    app.run(host='0.0.0.0', port=8000, debug=True)

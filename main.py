5dc6af4335dde9753e55797b7328872e8394270f
sungeun532 <55496333+sungeun532@users.noreply.github.com>
2019-10-16 02:56:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 import flask
 from flask import Flask, request, render_template
-from sklearn.externals import joblib
+from werkzeug.utils import secure_filename
 import numpy as np
-from scipy import misc
-import pysynth as ps      # 노트 만들면 wav파일로 저장
-from scipy.io import wavfile    # sample_rate, data 반환
+import pysynth as ps
+from scipy.io import wavfile
 
 app = Flask(__name__)
-app.debug = True
 
 
-# 메인 페이지 라우팅
-@app.route("/")
+@app.route('/')
 @app.route("/upload")
 def index():
     return render_template('index.html')
 
-@app.route('/uploader', methods=['POST'])
-def wav_freq_transform():
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def wav_transform():
     if request.method == 'POST':
-        # 업로드 파일 처리 분기
-        f = request.files['music']        
-        sr, data = scipy.io.wavfeil(f)
-                    
-        # 노트 추출
-        empty_notes = controller.NoteConvertor.convert(data)
+        f = request.files['file']
+        f.save(f'static/file1.wav')
+        sr, data = wavfile.read('static/file1.wav')
+
+        empty_notes = controller.NoteConvertor(data)
         notes = empty_notes.convertor()
         song = sum(notes, [])
-        
-        # 마르코프 추출
-        matrix = controller.MarcovMatrix(song)
-        start_note = ['c4', 4]
 
-        # 작곡 추출
+        matrix = controller.MarcovMatrix(song)
+        start_note = ['e4', 4]
+
         random_song = []
         for i in range(0, 100):
                 start_note = matrix.next_note(start_note)
@@ -40,9 +35,8 @@ def wav_freq_transform():
 
         ps.make_wav(random_song, fn='./random.wav')
 
-        return 
+        return render_template('wavplay.html')
 
-# Rest 등록
+
 if __name__ == '__main__':
-    # Flask 서비스 스타트
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(debug = True)

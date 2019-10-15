7bc105feafc559d78c2139e999f0ce3f42a0ad8b
sungeun532 <55496333+sungeun532@users.noreply.github.com>
2019-10-15 19:22:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.externals import joblib
 import numpy as np
 from scipy import misc
 import pysynth as ps      # 노트 만들면 wav파일로 저장
+from scipy.io import wavfile    # sample_rate, data 반환
 
 app = Flask(__name__)
 app.debug = True
@@ -19,12 +20,14 @@ def index():
 def wav_freq_transform():
     if request.method == 'POST':
         # 업로드 파일 처리 분기
-        f = request.files['music']
-#       if not file: return render_template('index.html', label="No Files")
-
+        f = request.files['music']        
+        sr, data = scipy.io.wavfeil(f)
+                    
         # 노트 추출
-        song = controller.NoteConvertor.convert(f)
-
+        empty_notes = controller.NoteConvertor.convert(data)
+        notes = empty_notes.convertor()
+        song = sum(notes, [])
+        
         # 마르코프 추출
         matrix = controller.MarcovMatrix(song)
         start_note = ['c4', 4]

d7e1ff0aab16ff5fb22d8e39ad4661cd99432f91
sungeun532 <55496333+sungeun532@users.noreply.github.com>
2019-10-27 04:36:08 +0900

    Update MarcovMatrix_integrated.py

--- controller/MarcovMatrix_integrated.py
+++ controller/MarcovMatrix_integrated.py
@@ -1,6 +1,3 @@
-
-
-
 class MarcovMatrix:
 
     def __init__(self, song=None):
@@ -10,8 +7,8 @@ class MarcovMatrix:
         self.uniq_durations = []
         self.pitch_index = {}
         self.dura_index = {}
-        self.Pitch = [][]
-        self.Durations = [][]
+        self.Pitch = []
+        self.Durations = []
 
         # song = [('c4', 32), ('c4', 16), ..]
         pitch = np.array(song, dtype=str)[:, 0] # ['c4', 'c4', ...]
@@ -21,15 +18,15 @@ class MarcovMatrix:
             
             
         # uniq_song은 start_note 추출
-        uniq_song = np.unique(song, axis=0).tolist() # [('c4', 16)]
-        uniq_pitch = np.unique(pitch).tolist() # ['c4', 'd4']
-        uniq_durations = np.unique(durations).tolist() 
+        self.uniq_song = np.unique(song, axis=0).tolist() # [('c4', 16)]
+        self.uniq_pitch = np.unique(pitch).tolist() # ['c4', 'd4']
+        self.uniq_durations = np.unique(durations).tolist() 
         
-        Pitch, pitch_index = self.matrixbuilder(uniq_pitch)
-        Durations, dura_index = self.matrixbuilder(uniq_durations)
+        self.Pitch, self.pitch_index = self.matrixbuilder(uniq_pitch)
+        self.Durations, self.dura_index = self.matrixbuilder(uniq_durations)
 
         for note in song:  # note ('c4', 32)
-            self.add(note, pitch_index, dura_index)
+            self.add(note, self.pitch_index, self.dura_index)
 
 
     def float2str(self, d):
@@ -39,7 +36,7 @@ class MarcovMatrix:
             return '%.2f' % float(d)
 
     def matrixbuilder(self, uniq_list):    
-        matrix = [][]
+        matrix = []
         index = {}
         
         for i in range(0, len(uniq_list)):
@@ -60,30 +57,30 @@ class MarcovMatrix:
         
         from_note = self.previous_note
          # pitch_index = {'c4' : 0, 'd4' : 1, ...}
-        Pitch[pitch_index[frome_note[0]]][pitch_index[to_note[0]]] += 1
-        Durations[dura_index[from_note[1]]][dura_index[to_note[1]]] += 1
+        self.Pitch[self.pitch_index[from_note[0]]][self.pitch_index[to_note[0]]] += 1
+        self.Durations[self.dura_index[from_note[1]]][self.dura_index[to_note[1]]] += 1
         self.previous_note = to_note
 
     def next_note(self, from_note):  # frome_note = ['c3', 16]
         from_note = list(from_note)
         from_note[1] = self.float2str(from_note[1])
         
-        pitch_indexnum = self.pitch_index[from_note[0]]  # 해당하는 행의 index번호
-        dura_indexnum = self.dura_index[from_note[1]]
+        pitch_rowindex = self.pitch_index[from_note[0]]  # 해당하는 행의 index번호
+        dura_rowindex = self.dura_index[from_note[1]]
         
-        pitch_indexnum = self.same_note_check(pitch_indexnum, pitch=True)  # 한 음정 반복되는 오류 검사
-        dura_indexnum = self.same_note_check(dura_indexnum, pitch=False)
+        pitch_rowindex = self.same_note_check(pitch_rowindex, pitch=True)  # 한 음정 반복되는 오류 검사
+        dura_rowindex = self.same_note_check(dura_rowindex, pitch=False)
         
-        pitch_row = Pitch[pitch_indexnum]
-        dura_row = Durations[dura_indexnum]
+        pitch_row = self.Pitch[pitch_rowindex]
+        dura_row = self.Durations[dura_rowindex]
         
-        pitch_new_index = self.select(pitch_row)
-        duration_new_index = self.select(dura_row)
+        pitch_new_ = self.select(pitch_row)
+        duration_new_ = self.select(dura_row)
         
-        if(pitch_new_index < 0 or duration_new_index < 0):
+        if(pitch_new_ < 0 or duration_new_ < 0):
             raise RuntimeError("impossible selection")
-        else 
-            return [uniq_pitch[pitch_new_index], float(uniq_durations[duration_new_index])]
+        else: 
+            return [self.uniq_pitch[pitch_new_], float(self.uniq_durations[duration_new_])]
         
     def select(self, row):
         
@@ -91,7 +88,7 @@ class MarcovMatrix:
         all_sum = sum(row)
         
         if all_sum == 0:
-            return random.randint(0, len(row)-1)
+            return random.randrange(0, len(row))
         else:
             randomly_number = random.randrange(1, all_sum+1)
             for i in range(0, len(row)):
@@ -101,33 +98,31 @@ class MarcovMatrix:
         raise RuntimeError("impossoble selection")            
         
             
-    def same_note_check(self, indexnum, pitch=True):
+    def same_note_check(self, rowindex, pitch=True):
         check = 0
-        test_index = indexnum
+        test_index = rowindex
         
         if pitch==True:
             
             while True:
-                for i in len(uniq_pitch):
-                    if Pitch[test_index][i]==0:
+                for i in range(len(self.uniq_pitch)):
+                    if self.Pitch[test_index][i]==0:
                         check += 1
             # check가 전체길이-1과 같으면 행렬요소값이 하나만 존재하고 나머지는 0
-                if check == (len(uniq_pitch)-1): 
-                    test_index = random.randrange(0, len(uniq_pitch))
+                if check == (len(self.uniq_pitch)-1): 
+                    test_index = random.randrange(0, len(self.uniq_pitch))
                     continue    
                 else:
                     return test_index                   
         else:
               while True:
-                for i in len(uniq_durations):
-                    if Durations[test_index][i]==0:
+                for i in range(len(self.uniq_durations)):
+                    if self.Durations[test_index][i]==0:
                         check += 1
             # check가 전체길이-1과 같으면 행렬요소값이 하나만 존재하고 나머지는 0
-                if check == (len(uniq_durations)-1): 
-                    test_index = random.randrange(0, len(uniq_durations))
+                if check == (len(self.uniq_durations)-1): 
+                    test_index = random.randrange(0, len(self.uniq_durations))
                     continue    
                 else:
                     return test_index    
-        
-
         

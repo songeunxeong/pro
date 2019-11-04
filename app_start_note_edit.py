7d949d24cf0969063d95b328a30f0fa4e24e0fba
sungeun532 <55496333+sungeun532@users.noreply.github.com>
2019-11-05 00:19:36 +0900

    Update app_start_note_edit.py

--- app_start_note_edit.py
+++ app_start_note_edit.py
@@ -19,13 +19,13 @@ def index():
 def wav_transform():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(f'static/file1.wav')
+        f.save(f'static/file.wav')
         try:
-            sr, data = wavfile.read('static/file1.wav')
+            sr, data = wavfile.read('static/file.wav')
         except ValueError:
             return render_template('error.html')  #wav파일외업로드시 error페이지로 이동
         
-        y, sr1 = librosa.load('static/file1.wav')
+        y, sr1 = librosa.load('static/file.wav')
         bpm, beats = librosa.beat.beat_track(y=y, sr=sr1)    #y? sr?
 
         convertor = controller.NoteConvertor(data, bpm)
@@ -33,13 +33,13 @@ def wav_transform():
         
         # start_note 수정
         composition_model = controller.MarcovMatrix(song)
-        candidate = composition_model.uniqu_song()
+        candidate = composition_model.uniq_song
         choice = random.randrange(0, len(candidate)-1)
         start_note = candidate[choice]
 
         random_song = []
         for i in range(0, 100):
-                start_note = matrix.next_note(start_note)
+                start_note = composition_model.next_note(start_note)
                 random_song.append(start_note)
 
         ps.make_wav(random_song, fn='./random.wav')

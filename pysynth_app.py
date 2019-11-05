5e95ce1f17753edf3f6b90def7a177fecc55636e
sungeun532 <55496333+sungeun532@users.noreply.github.com>
2019-11-06 02:50:24 +0900

    Update and rename plus_midi_app.py to pysynth_app.py

--- pysynth_app.py
+++ pysynth_app.py
@@ -1,12 +1,11 @@
 import flask
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
-import numpy as np
-# import pysynth as ps
 from scipy.io import wavfile
 import librosa
-from pyknon.genmidi import Midi
-from pyknon.music import NoteSeq, Note, Rest
+import random
+import pysynth as ps
+import controller
 
 app = Flask(__name__)
 
@@ -21,33 +20,32 @@ def index():
 def wav_transform():
     if request.method == 'POST':
         f = request.files['file']
+        random_length = request.form['length']
+        random_length = int(random_length)
         f.save(f'static/original_song.wav')
         try:
             sr, data = wavfile.read('static/original_song.wav')
         except ValueError:
             return render_template('error.html')  #wav파일외업로드시 error페이지로 이동
-        
+
         y, sr1 = librosa.load('static/original_song.wav')
-        bpm, beats = librosa.beat.beat_track(y=y, sr=sr1)    #y? sr?
+        bpm, beats = librosa.beat.beat_track(y=y, sr=sr1)
 
         convertor = controller.NoteTempoConvertor(data, bpm, sr)
         song = convertor.convert()
-        
-        # start_note 수정
+
         composition_model = controller.MarcovMatrix(song)
         candidate = composition_model.uniq_song
         choice = random.randrange(0, len(candidate)-1)
         start_note = candidate[choice]
 
         random_song = []
-        for i in range(0, 100):
+        for i in range(0, random_length):
                 start_note = composition_model.next_note(start_note)
                 random_song.append(start_note)
 
-#        ps.make_wav(random_song, fn='./random.wav')
-        midi = MakeMidi(random_song, bpm, "static/new_song.midi")
-        midi.makemidi()
-        
+        ps.make_wav(random_song, fn='static/random.wav')
+
         return render_template('wavplay.html')
 
 

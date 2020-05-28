1b5a8d58400a298ff220b6a5f6d9300fd3a1ab03
sungeun532 <55496333+sungeun532@users.noreply.github.com>
2020-05-29 05:18:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,34 +23,25 @@ def wav_transform():
     if request.method == 'POST':
         f = request.files['file']
         random_length = 100
-        f.save(f'static/original_song.wav')
+        f.save(f'static/song.wav')
         try:
-            sr, data = wavfile.read('static/original_song.wav')
+            sr, data = wavfile.read('static/song.wav')
         except ValueError:
             return render_template('error.html')  #wav파일외업로드시 error페이지로 이동
 
-        y, sr1 = librosa.load('static/original_song.wav')
+        y, sr1 = librosa.load('static/song.wav')
         bpm, beats = librosa.beat.beat_track(y=y, sr=sr1)
 
-        convertor = controller.NoteTempoConvertor(data, bpm, sr)
+        denoiser = controller.Denoising(data)
+        denoising_data = denoiser.denoising()
+        convertor = controller.NoteTempoConvertor(denoising_data, bpm, sr)
         song = convertor.convert()
         # global musicsheet_song
         global musicsheet_song
         musicsheet_song = convertor.musicsheet_song
 
-        composition_model = controller.MarcovMatrix(song)
-        candidate = composition_model.uniq_song
-        choice = random.randrange(0, len(candidate)-1)
-        start_note = candidate[choice]
-
-        ## 보류
-        random_song = []
-        for i in range(0, random_length):
-                start_note = composition_model.next_note(start_note)
-                random_song.append(start_note)
-
         ## song/random_song
-        midi = controller.MakeMidi(song, bpm, "static/random.midi")
+        midi = controller.MakeMidi(song, bpm, "static/song.midi")
         midi.makemidi()
         return render_template('midiplay.html')
 
@@ -61,7 +52,7 @@ def midi_play():
     pygame.mixer.init()
     freq, size, chan = pygame.mixer.get_init()
     BUFFER = 3072
-    filename = "static/random.midi"
+    filename = "static/song.midi"
     pygame.mixer.init(freq, size, chan, BUFFER)
 
     clock = pygame.time.Clock()
@@ -95,10 +86,10 @@ def make_musicsheet():
             n.duration.quarterLength = elm[1]
             s.append(n)
 
-    ## lilypond.exe 경로 변경하기        
+    ## lilypond.exe 경로 변경하기
     us = environment.UserSettings()
-    us['lilypondPath'] = 'C:/Users/17171/OneDrive/바탕 화면/LilyPond/usr/bin/lilypond.exe'
-    s.show('lily', fp='static/product')
+    us['lilypondPath'] = 'C:/Program Files (x86)/LilyPond/usr/bin/lilypond-windows.exe'
+    s.show('lily', fp='static/MusicSheet')
 
     return "nothing"
 
